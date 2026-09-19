cc40ea076e5d85f2ca4740810353470f1c236e7e
pedrooDevV <pedromplctitto@gmail.com>
2026-09-18 21:49:37 -0300

    Teste com backend deployado

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/LeoApi";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export async function agendarMentoriaApi(agendaId) {
   const token = localStorage.getItem("token");
